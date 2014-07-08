6cf56bc1f0413daf4811e65e088aa8f397d86167
Johan Spann <johan.spann@securitas-direct.com>
2014-07-08 12:25:56 +0200

    Changed the project structure of api

--- public/index.php
+++ public/index.php
@@ -1,67 +1,106 @@
 <?php
 
-/**
- * Very simple MVC structure
- */
-
-$loader = new \Phalcon\Loader();
-
-$loader->registerDirs(array('../apps/controllers/', '../apps/models/'));
-
-$loader->register();
-
-$di = new \Phalcon\DI();
-
-//Registering a router
-$di->set('router', 'Phalcon\Mvc\Router');
-
-//Registering a dispatcher
-$di->set('dispatcher', 'Phalcon\Mvc\Dispatcher');
-
-//Registering a Http\Response
-$di->set('response', 'Phalcon\Http\Response');
-
-//Registering a Http\Request
-$di->set('request', 'Phalcon\Http\Request');
-
-//Registering the view component
-$di->set('view', function(){
-	$view = new \Phalcon\Mvc\View();
-	$view->setViewsDir('../apps/views/');
-	return $view;
-});
-
-/** Init the database connection */
-$di->set('db', function(){
-   return new \Phalcon\Db\Adapter\Pdo\Sqlite(array(  // <- return
-      "dbname" => '../apps/data/database.sqlite'
-   ));
-});
-/*
-$di->set('db', function(){
-	return new \Phalcon\Db\Adapter\Pdo\Mysql(array(
-		"host" => "localhost:3306",
-		"username" => "root",
-		"password" => "",
-		"dbname" => "hejsan"
-	));
-});
-*/
-
-//Registering the Models-Metadata
-$di->set('modelsMetadata', 'Phalcon\Mvc\Model\Metadata\Memory');
-
-//Registering the Models Manager
-$di->set('modelsManager', 'Phalcon\Mvc\Model\Manager');
+error_reporting(E_ALL);
 
 try {
+
+	/**
+	 * Read the configuration
+	 */
+	$config = include(__DIR__."/../app/config/config.php");
+
+	$loader = new \Phalcon\Loader();
+
+	/**
+	 * We're a registering a set of directories taken from the configuration file
+	 */
+	$loader->registerDirs(
+		array(
+			$config->application->controllersDir,
+			$config->application->modelsDir
+		)
+	)->register();
+
+	/**
+	 * The FactoryDefault Dependency Injector automatically register the right services providing a full stack framework
+	 */
+	$di = new \Phalcon\DI\FactoryDefault();
+
+	/**
+	 * Include the application routes
+	 */
+	$di->set('router', function(){
+		return include(__DIR__."/../app/config/routes.php");
+	});
+
+	/**
+	 * The URL component is used to generate all kind of urls in the application
+	 */
+	$di->set('url', function() use ($config) {
+		$url = new \Phalcon\Mvc\Url();
+		$url->setBaseUri($config->application->baseUri);
+		return $url;
+	});
+
+	/**
+	 * Setting up the view component
+	 */
+	$di->set('view', function() use ($config) {
+		$view = new \Phalcon\Mvc\View();
+		$view->setViewsDir($config->application->viewsDir);
+		return $view;
+	});
+
+	/**
+	 * Database connection is created based in the parameters defined in the configuration file
+	 */
+	/*
+	$di->set('db', function() use ($config) {
+		return new \Phalcon\Db\Adapter\Pdo\Mysql(array(
+			"host" => $config->database->host,
+			"username" => $config->database->username,
+			"password" => $config->database->password,
+			"dbname" => $config->database->name
+		));
+	});
+	*/
+	/** Init the database connection */
+	$di->set('db', function(){
+	   return new \Phalcon\Db\Adapter\Pdo\Sqlite(array(  // <- return
+	      "dbname" => '../apps/data/database.sqlite'
+	   ));
+	});
+
+
+	/**
+	 * Register the flash service with custom CSS classes
+	 */
+	$di->set('flash', function(){
+		return new Phalcon\Flash\Direct(array(
+			'error' => 'alert alert-error',
+			'success' => 'alert alert-success',
+			'notice' => 'alert alert-info',
+		));
+	});
+
+	/**
+	 * Start the session the first time some component request the session service
+	 */
+	$di->set('session', function() {
+		$session = new \Phalcon\Session\Adapter\Files();
+		$session->start();
+		return $session;
+	});
+
+	/**
+	 * Handle the request
+	 */
 	$application = new \Phalcon\Mvc\Application();
 	$application->setDI($di);
 	echo $application->handle()->getContent();
-}
-catch(Phalcon\Exception $e){
 
-    /** Code to make 404-response working properly. */
+} catch (Phalcon\Exception $e) {
+	 /** Code to make 404-response working properly. */
 	echo $e->getMessage(); die; // <-  MAKE SURE TO HAVE 'DIE' HERE, ELSE NO ERROR MSG! =(
 
 	        // remove view contents from buffer
@@ -105,4 +144,8 @@ catch(Phalcon\Exception $e){
             ->setStatusCode($errorCode, null)
             ->setContent($contents)
             ->send();
+
+
+} catch (PDOException $e){
+	echo $e->getMessage();
 }
